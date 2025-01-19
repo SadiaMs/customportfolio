c5914124f1610da1596d87dfadf286f36793ef74
sadia-siddique <162547312+SadiaMs@users.noreply.github.com>
2025-01-19 16:43:46 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
           We are a passionate team of developers, designers, and creatives dedicated to building innovative web solutions. Our mission is to create user-friendly, responsive, and visually appealing websites that deliver exceptional user experiences.
         </p>
         <p className="about-us-description">
-          With a focus on the latest technologies and trends, we specialize in Next.js, React, Tailwind CSS, and more. Whether you're looking for a personal portfolio, an e-commerce platform, or a custom web solution, we are here to bring your ideas to life.
+          With a focus on the latest technologies and trends, we specialize in Next.js, React, Tailwind CSS, and more. Whether you are looking for a personal portfolio, an e-commerce platform, or a custom web solution, we are here to bring your ideas to life.
         </p>
         <p className="about-us-description">
           Our team thrives on collaboration, creativity, and constant learning, ensuring that we stay ahead of the curve in the fast-paced world of web development.
